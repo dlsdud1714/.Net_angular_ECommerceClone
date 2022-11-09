db94b17eaeb166125e1eb1134d1431b64b11cd75
InyoungPark <75140270+dlsdud1714@users.noreply.github.com>
2022-11-08 18:29:55 -0700

    Search

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -12,5 +12,9 @@ namespace Core.Specifications
         public int? BrandId {get;set;}
         public int? TypeId{get;set;}
         public string Sort{get;set;}
+        private string _search;
+        public string Search { 
+            get=>_search;
+            set=>_search=value.ToLower(); }
     }
 }
